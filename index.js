3af25124ac54f1022acb4ef1ba50c4de0825c1d1
Bergson <bergsonjr@icloud.com>
2019-04-23 12:05:40 -0300

    read matriz_dissimilaridade and convert to matriz[,]

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ const Professor = require('./class/Professor.js')
 const pathAluno = './files/Dados_Aluno.txt'
 const pathMatriz = './files/Matriz_Dissimilaridade.txt'
 
-
 function readFile(path) {
     return fs.readFileSync(path).toString()
 }
 
 async function main() {
     let arrayAlunos = []
+    let matrizContent = await readFile(pathMatriz)
     let fileContent = await readFile(pathAluno).replace(/\n/g, ';').split(';')
-
+    //converte conteúdo do arquivo para um array de Alunos
     for (let i = 0; i < fileContent.length; i++) {
         arrayAlunos.push(new Aluno(parseInt(fileContent[i].substring(0, 2)), parseInt(fileContent[i].substring(3, 5))))
     }
-    console.log(arrayAlunos)
+    //converte conteúdo do arquivo para uma matriz
+    let matrizDissimilaridade = matrizContent.split('\n').map((e) => { return e.split(' ') })
+    console.log(matrizDissimilaridade[0, 0][1])
+
 }
 
 main()
+
+//coluna -> cod_pesq

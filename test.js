61be39f691590f0bb5bde9ad620014ea990dd51e
Bergson <bergsonjr@icloud.com>
2019-04-23 12:51:34 -0300

    add two case of tests

--- test.js
+++ test.js
@@ -9,19 +9,29 @@ const Professor = require('./class/Professor.js')
 //files
 const pathAluno = './files/Dados_Aluno.txt'
 const pathMatriz = './files/Matriz_Dissimilaridade.txt'
+const pathAlunoTest = './files/Dados_Aluno_Test.txt'
 
 //Tests
 describe('Testes do TI de Grafos', async function () {
 
-    it('Leitura do arquivo de Alunos', async function () {
+    it('Leitura do arquivo de Alunos', async () => {
+        let condition = true
+        let result = await fs.readFileSync(pathAluno).toString()
+        await fs.appendFileSync(pathAlunoTest, result, (err) => { if (err) condition = false })
+        let result2 = await fs.readFileSync(pathAlunoTest).toString()
+        assert.deepEqual(result, result2) //-> verifica se o conteúdo do arquivo de teste é igual ao original
+        assert.deepEqual(true, condition) //-> verifica se ocorreu algum erro durante a leitura do arquivo
+    })
+    it('Deleta arquivo de teste de Alunos', async () => {
+        let condition = true
+        await fs.unlinkSync(pathAlunoTest, (err) => { if (err) condition = false })
+        assert.deepEqual(true, condition) //-> verifica se ocorreu algum erro ao deletar o arquivo
+    })
+    it('Leitura do arquivo de Matriz', async () => {
         assert.equal(true, true)
         //TODO
     })
-    it('Leitura do arquivo de Matriz', async function () {
-        assert.equal(true, true)
-        //TODO
-    })
-    it('Inserção de informações dos alunos', async function () {
+    it('Inserção de informações dos alunos', async () => {
         assert.equal(true, true)
         //TODO
     })

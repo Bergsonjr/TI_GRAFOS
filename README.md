c11da98840c19e425cf7f17f5252068f77288731
Bergson <bergsonjr@icloud.com>
2019-04-23 12:17:56 -0300

    add mocha to tests functions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TI_GRAFOS
 Trabalho integrado de grafos.
+
+Siga as instruções abaixo para buildar o ambiente e executar a aplicação:
+# npm install // npm i -> para baixar as dependências do projeto
+# node . // node index -> para executar o projeto
+# mocha -> para executar os testes do projeto
---
+++ test.js
@@ -0,0 +1,28 @@
+//libs node.js
+const fs = require('fs')
+const assert = require('assert')
+
+//classes
+const Aluno = require('./class/Aluno.js')
+const Professor = require('./class/Professor.js')
+
+//files
+const pathAluno = './files/Dados_Aluno.txt'
+const pathMatriz = './files/Matriz_Dissimilaridade.txt'
+
+//Tests
+describe('Testes do TI de Grafos', async function () {
+
+    it('Leitura do arquivo de Alunos', async function () {
+        assert.equal(true, true)
+        //TODO
+    })
+    it('Leitura do arquivo de Matriz', async function () {
+        assert.equal(true, true)
+        //TODO
+    })
+    it('Inserção de informações dos alunos', async function () {
+        assert.equal(true, true)
+        //TODO
+    })
+})
